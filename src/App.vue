44346f699f46f2eefd507cd9f6ef4962c2656ac5
Levi <110337030+LEI-LJ@users.noreply.github.com>
2023-12-20 17:13:15 +0800

    优化pinia结构

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    <VanButton type="primary"> 我是一个按钮 </VanButton>
+    <vanButton type="primary">你好</vanButton>
+    <button
+      @click="
+        userStore.setUser({
+          token: 'token',
+          id: '1121',
+          account: '1212',
+          mobile: '1212',
+          avatar: '1212'
+        })
+      "
+    >
+      登录
+    </button>
+    <button @click="userStore.delUser()">退出</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Button as VanButton } from 'vant'
+// import { useUserStore } from './stores/user'
+import { useUserStore } from './stores'
+// 模拟登录按钮
+const userStore = useUserStore()
+// const handleClick = () => {}
 </script>
-
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import pinia from './stores'
 import 'vant/lib/index.css'
 import '@/styles/main.scss'
 // 将出错信息显示在页面上
 import App from './App.vue'
 import router from './router'
 const app = createApp(App)
-
-app.use(createPinia())
+// const pinia = createPinia()
+// pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 //
 app.mount('#app')
