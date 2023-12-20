ff562919107029c9075250a84ab958bd3ccf68d8
Levi <110337030+LEI-LJ@users.noreply.github.com>
2023-12-20 10:53:20 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// 将出错信息显示在页面上
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
